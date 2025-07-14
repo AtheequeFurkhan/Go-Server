229cfe9a98961f6f128f6765240fbf7b2b6b273d
Atheeque <159514505+AtheequeFurkhan@users.noreply.github.com>
2025-07-14 11:54:08 +0530

    Added : Functions to Handle the Routes

--- static/main.go
+++ static/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+func helloHandler(w http.ResponseWriter,r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
 
+func formHandler(w http.ResponseWriter,r *http.Request){
+	if r.URL.Path != "/form"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Form!")
+
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("/static"))
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form",formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Printf("Starting servert at port 8080")
+	if error := http.ListenAndServe(":8080",nil); error != nil{
+		log.Fatal(error)
+	}
 }
